6606bb581ad68328864cc4b8556ff9aa7f939266
sergdz <sergeydzubenko1@gmail.com>
2022-04-26 13:57:38 +0300

    commit

--- main.js
+++ main.js
@@ -1,50 +1,55 @@
-let result = []
-let arr = []
 
 function func() {
+    let arr = [
+        "красный", "оранжевый", "желтый", "зеленый", "синий", "фиолетовый",
+        "белый", "черный", "голубой", "серый","Красный", "Оранжевый", 
+        "Желтый", "Зеленый", "Синий", "Фиолетовый",
+        "Белый", "Черный", "Голубой", "Серый"
+    ];
     let firstQuestion = prompt("Введите цвет");
-    if (firstQuestion.match(/["a-zA-Zа-яА-я"]+/)) {
-         result.push(car = {
+    if (firstQuestion === arr.find(string => string == firstQuestion)) {
+         car = {
             color: firstQuestion,
-        })
+        }
     }
     else {
         alert("Не коректный цвет");
         
     }
-    let secondQuestion = prompt("Введите год");
-    if (secondQuestion.match(/[0-9]+/)) {
-         result.push(car = {
+    let secondQuestion = parseInt(prompt("Введите год"));
+    if (secondQuestion >= 1940 && secondQuestion <=2022) {
+         car = {
             year: secondQuestion,
-        })
+        }
     }
     else {
         alert("Не верный год");
         
     }
-    let thirdQuestion = prompt("Введите цену");
-    if (thirdQuestion.match(/[0-9]+/)) {
-        result.push(car = {
+    let thirdQuestion = parseInt(prompt("Введите цену"));
+    if (thirdQuestion > 0 && thirdQuestion < 1000000) {
+        car = {
             price: thirdQuestion,
-        })
+        };
     }
     else {
         alert("Введите коректную цену");
     }
-    let fourthQuestion = prompt("Введите пробег");
-    if (fourthQuestion.match(/[0-9]+/)) {
-        result.push(car = {
+    let fourthQuestion = parseInt(prompt("Введите пробег"));
+    if (fourthQuestion > 0 && fourthQuestion < 500000) {
+        car = {
             mileage: fourthQuestion,
-        })
+        }
     }
     else {
-        alert("Введите коректный пробег")
-    }
+        alert("Введите коректный пробег");
+    };
+    let body = ["Седан", "Универсал", "Хэтчбек", "Лифтбек", "Купе", "Лимузин", "Кабриолет", "Внедорожник"  ];
     let fifthQuestion = prompt("Введите тип кузова");
-    if (fifthQuestion.match(/[a-zA-Zа-яА-я]+/)) {
-        result.push(car = {
+    if (fifthQuestion == body.find(string => string === fifthQuestion)) {
+        car = {
             bodyType: fifthQuestion,
-        })
+        };
     } else {
         alert("Тип кузова указан не верно");
     }
@@ -55,12 +60,8 @@ function func() {
         mileage: fourthQuestion,
         bodyType: fifthQuestion,
 
-    }
-    result.push(car)
-
-
-
-}
-func()
+    };
+};
+func();
 console.log(car);
 console.log(`Цвет : ${car.color}. Год выпуска : ${car.year} г. Цена : ${car.price} $ Пробег : ${car.mileage} км. Тип кузова : ${car.bodyType}`);
